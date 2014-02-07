edc632a1079cdb3a905169a8ddd7be77af39157b
@jonkiddy <jon.kiddy@gmail.com>
2014-02-07 18:47:19 -0500

    Go team 4930!

--- myRobot.cpp
+++ myRobot.cpp
@@ -8,7 +8,7 @@
  * @goal (2) Drive robot using Tank Drive
  * @goal (3) Move robot forwad during autonomous mode.
  * @goal (4) Get C++ code compiled and running on the 2013 robot's cRIO.
- * @author Jon Kiddy
+ * @author Programming Team
  * @version 0.0.2 2014-02-01
  */ 
 class Robot4930 : public SimpleRobot
