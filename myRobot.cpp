33fbfc4033a4b78e47e2c43dfeb8ca85aa36b2a8
@jonkiddy <jon.kiddy@gmail.com>
2014-02-07 18:46:15 -0500

    Updated comments. Now using two joysticks.

--- myRobot.cpp
+++ myRobot.cpp
@@ -4,8 +4,8 @@
  * FRC Robot Team #4930 The Electric Mayhem
  * @link www.nicholsrobotics.org
  * @uses WindRiver FRC SimpleRobot Class
- * @goal (1) Initalize motors and joystick.
- * @goal (2) Drive robot using Tank Drive with a USB Logitech Dual Action Gamepad.
+ * @goal (1) Initalize motors and two joysticks.
+ * @goal (2) Drive robot using Tank Drive
  * @goal (3) Move robot forwad during autonomous mode.
  * @goal (4) Get C++ code compiled and running on the 2013 robot's cRIO.
  * @author Jon Kiddy
